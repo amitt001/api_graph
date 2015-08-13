0f339364d797d44760778a8d1f8ba40b6d4e0e8c
amitt001 <mail2amit19@gmail.com>
2015-08-13 22:06:35 +0530

    logserver api done, api_logger done

--- api_logger.py
+++ api_logger.py
@@ -3,30 +3,32 @@
 import sys
 import json
 import time
-import pprint
 import datetime
+import requests
 from collections import defaultdict
 
+import redis
 import MySQLdb as mdb
 
 import get_urls
+import config
 
+"""
+Notes: commit after 1 minute
+"""
 
-#urls = json.load(open('urls', 'r')).keys()
 
 try:
-    con = mdb.connect('localhost', 'amit', 'pass', 'testdb');
+    con = mdb.connect(config.HOST, config.USER, config.PASSWORD, config.DB_NAME)
     cur = con.cursor()
-    #{"2015-08-06 09:05": {"200": {"http://api.frankly.me/timeline/user": 6964.552999999999
-    sql = """CREATE TABLE IF NOT EXISTS API_PERFORM (
+    sql = """CREATE TABLE IF NOT EXISTS %s (
         DATE_TIME  DATETIME,
         STATUS  CHAR(5),
         URL VARCHAR(256),
         RESPONSE_TIME FLOAT,
         COUNT INT,
         PRIMARY KEY (DATE_TIME, STATUS, URL) )
-    """
-
+        """ % (config.TABLE_RESPONSE)
     cur.execute(sql)
 
 except mdb.Error, e:
@@ -34,46 +36,28 @@ except mdb.Error, e:
     con.close()
     sys.exit(1)
 
+#Redis connection
+try:
+    r = redis.Redis(
+        host=config.REDIS_SERVER,
+        port=config.REDIS_PORT)
+
+except redis.ConnectionError:
+    print('Redis Connection Error')
+
+tmp_count = 0
+
 if __name__ == '__main__':
-    fileBytePos = 0
-    minute_flg = 0
-    response_by_minute = defaultdict(dict)
-    response_by_minute_count = defaultdict(dict)
-    response_count = defaultdict(dict)
-    pp = pprint.PrettyPrinter()
-    url_count = 0
-    tmp_time  = ''
-
     while True:
-        inFile = open('./flask-timelog.txt','r')
-        inFile.seek(fileBytePos)
-        data = inFile.readline()
         
-        if data[0].isdigit():
-            i = data.split('::')
-            rsp_time, rsp, url, date_time = i[0].strip(), i[1].split()[0], get_urls.process_url(i[3]), i[-1].split('.')[0][:-3].strip()
-            #defaultdict initialize if key already not there
+        rsp = requests.get(config.URL)
+        response_by_minute = rsp.json()['log_by_min']
+        response_by_minute_count = rsp.json()['log_by_count']
 
-            if not response_by_minute.get(date_time, {}).get(rsp):
-                response_by_minute[date_time][rsp] = defaultdict(dict)
-                response_by_minute_count[date_time][rsp] = defaultdict(int)
-
-            response_by_minute[date_time][rsp][url] = response_by_minute.get(date_time,{}).get(rsp,{}).get(url,0) + float("{0:.4f}".format(float(rsp_time.split(':')[-1])))
-            #stores count of url with status in 1 minute period
-            response_by_minute_count[date_time][rsp][url] += 1
-            response_count[url][rsp] = response_count.get(url, {}).get(rsp, 0) + 1
-            #cur.execute('REPLACE INTO API_PERFORM VALUES (%s, %s, %s, %s)', (date_time, rsp, url, response_by_minute[date_time][rsp][url]))
-            cur.execute('INSERT INTO API_PERFORM (DATE_TIME, STATUS, URL, RESPONSE_TIME, COUNT) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE RESPONSE_TIME =  VALUES(RESPONSE_TIME), COUNT =  VALUES(COUNT)', (date_time, rsp, url, response_by_minute[date_time][rsp][url], response_by_minute_count[date_time][rsp][url]))
-            con.commit();
-
-        fileBytePos = inFile.tell()
-        inFile.close()
-        #for development remove it once deployed
-        print('current: {0} -> {1}'.format(fileBytePos, 386440877) )
-        if fileBytePos >= 386440877:
-            break
-
-    with open('api_perform', 'w') as o:
-        json.dump(response_by_minute, o)
-    with open('api_status', 'w') as b:
-        json.dump(response_count, b)
+        for date_time in response_by_minute.keys():
+            for rsp in response_by_minute[date_time].keys():
+                for url, rsp_tym in response_by_minute[date_time][rsp].iteritems():
+                    print (date_time, rsp, url, rsp_tym, response_by_minute_count[date_time][rsp][url])
+                    cur.execute('INSERT INTO API_PERFORM_ONE (DATE_TIME, STATUS, URL, RESPONSE_TIME, COUNT) VALUES (%s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE RESPONSE_TIME =  VALUES(RESPONSE_TIME), COUNT =  VALUES(COUNT)' , (date_time, rsp, url, rsp_tym, response_by_minute_count[date_time][rsp][url]))
+                    
+        con.commit()
---
+++ logserver_api.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import sys
+import json
+import time
+import pprint
+import datetime
+from collections import defaultdict
+
+import redis
+from flask import Flask, jsonify
+
+import get_urls
+import config
+
+app = Flask(__name__ , static_url_path='')
+
+
+#Redis connection
+try:
+    r = redis.Redis(
+        host=config.REDIS_SERVER,
+        port=config.REDIS_PORT)
+except redis.ConnectionError:
+    print('Redis Connection Error')
+
+@app.route('/')
+def index():
+    return 'Online'
+
+@app.route('/get/log')
+@app.route('/get/log/')
+def get_logs():
+    fileBytePos = 0
+    data_count = 500
+    response_by_minute = defaultdict(dict)
+    response_by_minute_count = defaultdict(dict)
+    response_count = defaultdict(dict)
+    tmp_time  = 'null'
+    #save file byte position in Redis if already exists get position
+    while True:
+        inFile = open('./flask-timelog.txt','r')
+        if not r.hget(config.REDIS_HNAME, 'fileBytePos'):
+            r.hset(config.REDIS_HNAME, 'fileBytePos', fileBytePos)
+        fileBytePos = int(r.hget(config.REDIS_HNAME, 'fileBytePos'))
+
+        inFile.seek(fileBytePos)
+        data = inFile.readline()
+        if data[0].isdigit():
+            i = data.split('::')
+            rsp_time, rsp, url, date_time = i[0].strip(), i[1].split()[0], get_urls.process_url(i[3]), i[-1].split('.')[0][:-3].strip()
+
+            if tmp_time == 'null':
+                tmp_time = date_time
+
+            if tmp_time != date_time:
+                break
+                
+            #defaultdict initialize if key already not there
+            if not response_by_minute.get(date_time, {}).get(rsp):
+                response_by_minute[date_time][rsp] = defaultdict(dict)
+                response_by_minute_count[date_time][rsp] = defaultdict(int)
+
+            response_by_minute[date_time][rsp][url] = response_by_minute.get(date_time,{}).get(rsp,{}).get(url,0) + float("{0:.4f}".format(float(rsp_time.split(':')[-1])))
+            #stores count of url with status in 1 minute period
+            response_by_minute_count[date_time][rsp][url] += 1
+            response_count[url][rsp] = response_count.get(url, {}).get(rsp, 0) + 1
+
+        fileBytePos = inFile.tell()
+        r.hset(config.REDIS_HNAME, 'fileBytePos', fileBytePos)
+        inFile.close()
+
+        data_count -= 1
+
+    return jsonify({'log_by_min': response_by_minute, 'log_by_count': response_by_minute_count}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000)
